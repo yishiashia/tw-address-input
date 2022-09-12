cc4e094f1dbe0197167bac72d3051dae3987b001
yishiashia <yishiashia@gmail.com>
2022-09-12 17:20:27 +0800

    feat: apply cities / towns data in dropdown menu

--- src/twAddress.ts
+++ src/twAddress.ts
@@ -1,16 +1,37 @@
 import 'wc-dropdown'
 import addressStyle from './twAddress.scss'
 
+import cities from './cities'
+
+interface Dropdown extends HTMLElement
+{
+    get options(): Array<{ name: string; value: string; }>;
+    set options(val: Array<{ name: string; value: string; }>);
+}
 export default class TWAddress extends HTMLElement {
+  #rootRef: HTMLElement | null
+  #cityRef: Dropdown | null
+  #townRef: Dropdown | null
+
   constructor () {
     super()
     this.attachShadow({ mode: 'open' })
+
+    this.#rootRef = null
+    this.#cityRef = null
+    this.#townRef = null
   }
 
   connectedCallback () {
     if (this.shadowRoot !== null) {
       // DOM
       this.shadowRoot.innerHTML = this.render()
+      this.#cityRef = this.shadowRoot.querySelector('.city-menu')
+      this.#townRef = this.shadowRoot.querySelector('.town-menu')
+
+      if (this.#cityRef !== null) {
+        this.#cityRef.options = cities
+      }
 
       // Style
       this.style.display = 'block'
@@ -31,8 +52,9 @@ export default class TWAddress extends HTMLElement {
           <dropdown-menu
             class="menu city-menu"
             placeholder="請選擇縣市"
-            ></dropdown-menu>
-            <dropdown-menu
+            maxitems="5"
+          ></dropdown-menu>
+          <dropdown-menu
             class="menu town-menu"
             placeholder="請選擇鄉/鎮/市/區"
           ></dropdown-menu>
