33f4849ad4d303156f5d83f318896888491ebeb7
yishiashia <yishiashia@gmail.com>
2022-09-12 17:18:01 +0800

    feat: add Taiwan cities, towns and zip data
    Reference: the data is origin from the Open Government Data
    (https://data.gov.tw/)

---
+++ src/cities.ts
@@ -0,0 +1,90 @@
+export default [
+  {
+    value: 'A',
+    name: '臺北市'
+  },
+  {
+    value: 'B',
+    name: '臺中市'
+  },
+  {
+    value: 'C',
+    name: '基隆市'
+  },
+  {
+    value: 'D',
+    name: '臺南市'
+  },
+  {
+    value: 'E',
+    name: '高雄市'
+  },
+  {
+    value: 'F',
+    name: '新北市'
+  },
+  {
+    value: 'G',
+    name: '宜蘭縣'
+  },
+  {
+    value: 'H',
+    name: '桃園市'
+  },
+  {
+    value: 'I',
+    name: '嘉義市'
+  },
+  {
+    value: 'J',
+    name: '新竹縣'
+  },
+  {
+    value: 'K',
+    name: '苗栗縣'
+  },
+  {
+    value: 'M',
+    name: '南投縣'
+  },
+  {
+    value: 'N',
+    name: '彰化縣'
+  },
+  {
+    value: 'O',
+    name: '新竹市'
+  },
+  {
+    value: 'P',
+    name: '雲林縣'
+  },
+  {
+    value: 'Q',
+    name: '嘉義縣'
+  },
+  {
+    value: 'T',
+    name: '屏東縣'
+  },
+  {
+    value: 'U',
+    name: '花蓮縣'
+  },
+  {
+    value: 'V',
+    name: '臺東縣'
+  },
+  {
+    value: 'W',
+    name: '金門縣'
+  },
+  {
+    value: 'X',
+    name: '澎湖縣'
+  },
+  {
+    value: 'Z',
+    name: '連江縣'
+  }
+]
