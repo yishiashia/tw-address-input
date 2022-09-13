ced4284885c51579c9513f74815525d882c30739
yishiashia <yishiashia@gmail.com>
2022-09-13 17:16:05 +0800

    feat: implement unit test cases

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,14 @@ module.exports = {
   "testEnvironment": "jsdom",
   "moduleNameMapper": {
     "^.+\\.(css|less|scss)$": "babel-jest"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/types/*',
+    '!src/index.ts',
+    '!src/cities.ts',
+    '!src/towns.ts',
+    '!src/zips.ts'
+  ]
 }
---
+++ tests/units/twAddress.spec.ts
@@ -0,0 +1,73 @@
+import '@testing-library/jest-dom';
+import { fireEvent } from '@testing-library/dom';
+import TWAddress from '../../src/twAddress'
+
+describe('twAddress.ts', () => {
+  window.customElements.define('tw-address', TWAddress);
+  // Render test cases
+  test('Must render props.placeholder when passed', () => {
+    document.body.innerHTML = `
+      <tw-address></tw-address>
+    `
+    const customElement = document.querySelector('tw-address');
+    expect(customElement === null).toBeFalsy();
+    if(customElement !== null && customElement.shadowRoot !== null) {
+      const cityMenu = customElement.shadowRoot.querySelector('.city-menu')
+      expect(cityMenu !== null)
+    } else {
+      fail("shadow dom not mount")
+    }
+  });
+
+  test('test unmount customElement', () => {
+    document.body.innerHTML = `
+    <tw-address></tw-address>
+    `
+    const customElement = document.querySelector('tw-address') as TWAddress;
+    if (customElement !== null) {
+      document.body.removeChild(customElement)
+    }
+  })
+
+  test('test fill in an address', async () => {
+    document.body.innerHTML = `
+    <tw-address name="deliveryAddr"></tw-address>
+    `
+    const customElement = document.querySelector('tw-address') as TWAddress;
+    if (customElement !== null) {
+      if (customElement.shadowRoot !== null) {
+        const cityMenu = customElement.shadowRoot.querySelector('.city-menu') as any
+        const townMenu = customElement.shadowRoot.querySelector('.town-menu') as any
+        const addrInput = customElement.shadowRoot.querySelector('.address-input') as HTMLInputElement
+        if (cityMenu !== null) {
+          cityMenu.setSelectedOption(5)
+          expect(customElement.value.city?.value).toBe('F')
+          if (townMenu !== null) {
+            townMenu.setSelectedOption(5)
+            expect(customElement.value.town?.value).toBe('F07')
+            if (addrInput !== null) {
+              const testStr = "blablabla"
+              addrInput.value = testStr
+              await fireEvent.keyUp(addrInput, {
+                key: "a",
+                code: "KeyA",
+                keyCode: 65,
+                charCode: 65
+              })
+              expect(customElement.value.address).toBe(testStr)
+            } else {
+              fail('address input not existed')
+            }
+          } else {
+            fail('town menu not existed')
+          }
+        } else {
+          fail('city menu not existed')
+        }
+      } else {
+        fail('shadowDOM not mounted')
+      }
+    }
+  })
+
+});
